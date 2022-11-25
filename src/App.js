01142baaaf5b9b8201bc682e41b9df6b68a3a467
Onur Kağan Coşkun <79399112+Okaca@users.noreply.github.com>
2022-11-25 21:51:20 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import BasicTextFields from './components/TextFileTemplate';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Add Candidate</h1>
+      <BasicTextFields/>
+    </div>
+  );
+}
+
+export default App;
